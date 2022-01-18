fcebbdded93d978c8b1febcbefbefab113346560
Alexander Smirnov <asmirnov@codero.io>
2022-01-18 17:03:52 +0600

    PEJT-23: changed configuration

--- src/main/resources/db/changelog/migration/create_table_letter.sql
+++ src/main/resources/db/changelog/migration/create_table_letter.sql
@@ -1,9 +1,9 @@
 create table if not exists letter (
   id uuid primary key,
-  raw_letter jsonb not null,
-  sender varchar(250) not null,
-  content varchar(250),
-  receiver varchar(250) not null,
   post_office_id serial not null,
-  time_stamp timestamp
+  sender varchar(250) not null,
+  receiver varchar(250) not null,
+  time_stamp timestamp,
+  raw_letter jsonb not null,
+  content varchar(250)
 );
